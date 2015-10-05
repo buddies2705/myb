7d0079ee33a09adc686865a990376ba1411f0cc6
Gaurav <buddies2705@gmail.com>
2015-10-06 00:19:19 +0530

    qb save

--- qb/__init__.py
+++ qb/__init__.py
@@ -7,6 +7,7 @@ from pymongo import MongoClient
 app=Flask(__name__)
 
 client = MongoClient()
-
+import qb.views.shopifycontroller
 import qb.views.qbController
 import qb.database.mongohelper
+import qb.views.maincontroller
--- qb/views/qbController.py
+++ qb/views/qbController.py
@@ -1,16 +1,15 @@
 import json
 import sys
+from qb.views.Comparehelper import Comparehelper
+import qb.database.mongohelper as database
 
 __author__ = 'gaurav'
 
 import re
-from compiler.ast import  flatten
 from qb import app
 from flask import Flask, request, render_template, redirect
 import requests.packages.urllib3
 from qb.models import QuickBooks
-import qb.database.mongohelper as database
-
 # 405455666
 # 1425017975
 
@@ -23,11 +22,14 @@ map2 = {'consumer_key': 'qyprdMYtVFGlwgkX15mGXVb1CLRpuz',
         }
 requests.packages.urllib3.disable_warnings()
 
+# creating Object
+comparehelper = Comparehelper()
 
-@app.route("/main", methods=['GET', 'POST'])
-def main():
+
+@app.route("/qbmain", methods=['GET', 'POST'])
+def qbmain():
     if request.method == "GET":
-        return render_template('main.html')
+        return render_template('quickbook.html')
     if request.method == 'POST':
         try:
             data = request.form
@@ -37,71 +39,41 @@ def main():
             map2.update({'access_token': data.get('qbaccesstoken_2')})
             map2.update({'access_token_secret': data.get('qbaccesstokensecret_2')})
             map2.update({'company_id': data.get('companyid_2')})
-            enities=data.getlist("entities")
+            enities = data.getlist("entities")
             qb1 = QuickBooks.QuickBooks(**map)
             qb1.create_session()
             qb2 = QuickBooks.QuickBooks(**map2)
             qb2.create_session()
-            theresult=[]
+            theresult = []
             for values in enities:
                 list1 = qb1.query_objects(values)
                 if list1:
-                    database.mongohelper.insertindatabse("system_data","qb",list1)
+                    database.mongohelper.insertindatabse("system_data", "qb", list1)
                 list2 = qb2.query_objects(values)
                 if list2:
-                    database.mongohelper.insertindatabse("system_data","qb",list2)
+                    database.mongohelper.insertindatabse("system_data", "qb", list2)
                 if len(list1) < len(list2):
-                    result = compare(list1, list2,values)
+                    result = compare(list1, list2, values)
                 else:
-                    result = compare(list2, list1,values)
-                filterresults(result)
+                    result = compare(list2, list1, values)
+                comparehelper.filterresults(result)
                 theresult.append(result)
-            theresult=filter(None,theresult)
+            theresult = filter(None, theresult)
             return render_template('result.html', theresult=theresult)
-        except :
+        except:
             print  sys.exc_info()
-            return redirect("/main")
+            return redirect("/qbmain")
 
 
 
-def filterresults(inputlist):
-    i=0
-    while(i<len(inputlist)):
-        inputlist[i]=[e for e in inputlist[i] if e]
-        inputlist[i]=flatten(inputlist[i])
-        i += 1
-
-
-def compare(list1, list2,entity):
+def compare(list1, list2, entity):
     i = 0
     mainlist = []
-    while (i < len(list1)):
-        comparelist = compareDictOfDict(list1[i], list2[i])
-        comparelist.insert(0, "the "+entity + " ID is in qb1 is " + str(list1[i].get("Id")) + " and in qb2 is " + str(
+    while i < len(list1):
+        comparelist = comparehelper.compareDictOfDict(list1[i], list2[i])
+        comparelist.insert(0, "the " + entity + " ID is in qb1 is " + str(list1[i].get("Id")) + " and in qb2 is " + str(
             list2[i].get("Id")))
         mainlist.append(comparelist)
-        i = i + 1
+        i += 1
     return mainlist
 
-
-def compareDictOfDict(dict1, dict2):
-    resultList = []
-    for keys in dict1:
-        # ignoring Time Stampe
-        if "id" in str(keys).lower() or ifTimeStamp(str(dict1.get(keys))) != None:
-            continue
-        if not isinstance(dict1.get(keys), dict):
-            if dict1.get(keys) != dict2.get(keys):
-                resultList.append(
-                    "the value of " + str(keys).upper() + " in qb1 is " + str(dict1.get(keys)).upper() + " but in qb2 is " + str(
-                        dict2.get(keys)).upper())
-        elif isinstance(dict1.get(keys), dict) and dict2.get(keys) != None:
-            resultList.append(compareDictOfDict(dict1.get(keys), dict2.get(keys)))
-        else:
-            resultList.append("the value of " + keys + " is present in qb1 but not in qb2")
-    return resultList
-
-
-def ifTimeStamp(str):
-    pattern = re.compile('\d\d\d\d-\d\d-\d\d(T)\d\d:\d\d:\d\d-\d\d:\d\d')
-    return re.match(pattern, str)
